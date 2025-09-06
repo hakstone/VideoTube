f75a040a7a47797ab06d231305c9b8189d64259e
simplest-ayush <ujjwal301004@gmail.com>
2025-09-07 04:20:00 +0530

    fixed a typo

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory, removeFromWatchHistory } from "../controllers/user.controller.js";
-import { upload } from '../middlewares/multer.milldeware.js'
+import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const router = Router()
