48c6d04c97df4053c9aa7145cfd3958ca92dc5a2
simplest-ayush <ujjwal301004@gmail.com>
2025-09-07 22:35:46 +0530

    deployed backend on render (hence changed the url)

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const API = axios.create({
-    baseURL: "https://video-tube-bay.vercel.app/api/v1",
+    // baseURL: "https://video-tube-bay.vercel.app/api/v1",
+    baseURL: "https://videotube-lo7n.onrender.com/api/v1",
     withCredentials: true,
 });
 
