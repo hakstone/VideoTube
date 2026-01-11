18189971b2aeb0692bd9be7548ee77907dee6020
Harsh Gupta <9045harshgupta@gmail.com>
2026-01-11 23:51:41 +0530

    Create proper vercel.js handler for backend

---
+++ backend/vercel.js
@@ -0,0 +1,12 @@
+import connectDB from './src/db/index.js';
+import { app } from './src/app.js';
+
+let dbConnected = false;
+
+export default async function handler(req, res) {
+    if (!dbConnected) {
+        await connectDB();
+        dbConnected = true;
+    }
+    return app(req, res);
+}
